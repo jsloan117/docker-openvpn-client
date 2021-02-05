4e0ea11cb57420505a8de26d791b7918b2432f76
Jonathan Sloan <jsloan117@gmail.com>
2021-02-04 20:58:39 -0600

    dev (#9)
    * major changes importing most of upstreams 3.0 code
    
    * updating to alpine v3.13

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
+
+#Network check
 # Ping uses both exit codes 1 and 2. Exit code 2 cannot be used for docker health checks,
 # therefore we use this script to catch error code 2
-HOST=$HEALTH_CHECK_HOST
+HOST=${HEALTH_CHECK_HOST}
 
-if [ -z "$HOST" ]
+if [[ -z "$HOST" ]]
 then
-    echo "Host  not set! Set env 'HEATH_CHECK_HOST'. For now, using default google.com"
+    echo "Host  not set! Set env 'HEALTH_CHECK_HOST'. For now, using default google.com"
     HOST="google.com"
 fi
 
 ping -c 1 $HOST
 STATUS=$?
-if [ $STATUS -ne 0 ]
+if [[ ${STATUS} -ne 0 ]]
 then
     echo "Network is down"
+    INTERFACE=$(ls -d /sys/class/net/tun*)
+    ISINTERFACE=$?
+
+    if [[ ${ISINTERFACE} -ne 0 ]]
+    then
+        echo "TUN Interface not found"
+        exit 1
+    fi
+
+    echo "Resetting TUN"
+    ip link set "${INTERFACE}" down
+    sleep 1
+    ip link set "${INTERFACE}" up
+    echo "Sent kill SIGUSR1 to openvpn"
+    pkill -SIGUSR1 openvpn
+    sleep 20
+fi
+
+ping -c 1 $HOST
+STATUS=$?
+if [[ ${STATUS} -ne 0 ]]
+then
+    echo "Network is still down"
     exit 1
 fi
 
 echo "Network is up"
+
+#Service check
+
+if ! pgrep openvpn; then
+    echo "Openvpn process not running"
+    exit 1
+fi
+
+echo "Openvpn process is running"
 exit 0
