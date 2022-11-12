f2e7cde015aa8b9a53955827bf3b5956b5bb72ea
Jonathan Sloan <jsloan117@gmail.com>
2022-11-12 18:36:00 -0500

    add back in update-port.sh for pia. replace ip route add w/ ip route replace to help prevent RTNETLINK file exists errors
    Signed-off-by: Jonathan Sloan <jsloan117@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -146,7 +146,9 @@ if [[ -n "${LOCAL_NETWORK}" ]]; then
   if [[ -n "${GW}" ]] && [[ -n "${INT}" ]]; then
     for localNet in ${LOCAL_NETWORK//,/ }; do
       echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
-      /sbin/ip route add "${localNet}" via "${GW}" dev "${INT}"
+      # Using `ip route replace` so that the command does not fail with
+      # `RTNETLINK answers: File exists` when the route already exists
+      /sbin/ip route replace "${localNet}" via "${GW}" dev "${INT}"
       if [[ "${ENABLE_UFW,,}" == "true" ]]; then
         if [[ -n "${UFW_EXTRA_PORTS}" ]]; then
           for port in ${UFW_EXTRA_PORTS//,/ }; do
