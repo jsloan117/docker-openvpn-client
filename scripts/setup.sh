8f0fecc3b2070f72ca84f166dd362ad75ee85a65
Jonathan Sloan <jsloan117@gmail.com>
2022-11-20 15:23:25 -0500

    update to call ufw.sh after openvpn runs
    Signed-off-by: Jonathan Sloan <jsloan117@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -101,66 +101,67 @@ if ! mountpoint -q "$CHOSEN_OPENVPN_CONFIG"; then
   fi
 fi
 
-# if we use UFW or the LOCAL_NETWORK we need to grab network config info
-if [[ "${ENABLE_UFW,,}" == "true" ]] || [[ -n "${LOCAL_NETWORK}" ]]; then
-  eval "$(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')"
-  # If we use UFW_ALLOW_GW_NET along with ENABLE_UFW we need to know what our netmask CIDR is
-  if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ "${UFW_ALLOW_GW_NET,,}" == "true" ]]; then
-    eval "$(/sbin/ip route list dev "${INT}" | awk '{if($5=="link"){print "GW_CIDR="$1; exit}}')"
-  fi
-fi
+# # if we use UFW or the LOCAL_NETWORK we need to grab network config info
+# if [[ "${ENABLE_UFW,,}" == "true" ]] || [[ -n "${LOCAL_NETWORK}" ]]; then
+#   eval "$(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')"
+#   # If we use UFW_ALLOW_GW_NET along with ENABLE_UFW we need to know what our netmask CIDR is
+#   if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ "${UFW_ALLOW_GW_NET,,}" == "true" ]]; then
+#     eval "$(/sbin/ip route list dev "${INT}" | awk '{if($5=="link"){print "GW_CIDR="$1; exit}}')"
+#   fi
+# fi
 
-# open port to any address
-function ufwAllowPort {
-  portNum=${1}
-  if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ -n "${portNum}" ]]; then
-    echo "allowing ${portNum} through the firewall"
-    ufw allow "${portNum}"
-  fi
-}
+# # open port to any address
+# function ufwAllowPort {
+#   portNum=${1}
+#   if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ -n "${portNum}" ]]; then
+#     echo "allowing ${portNum} through the firewall"
+#     ufw allow "${portNum}"
+#   fi
+# }
 
-# open port to specific address
-function ufwAllowPortLong {
-  portNum=${1}
-  sourceAddress=${2}
+# # open port to specific address
+# function ufwAllowPortLong {
+#   portNum=${1}
+#   sourceAddress=${2}
 
-  if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ -n "${portNum}" ]] && [[ -n "${sourceAddress}" ]]; then
-    echo "allowing ${sourceAddress} through the firewall to port ${portNum}"
-    ufw allow from "${sourceAddress}" to any port "${portNum}"
-  fi
-}
+#   if [[ "${ENABLE_UFW,,}" == "true" ]] && [[ -n "${portNum}" ]] && [[ -n "${sourceAddress}" ]]; then
+#     echo "allowing ${sourceAddress} through the firewall to port ${portNum}"
+#     ufw allow from "${sourceAddress}" to any port "${portNum}"
+#   fi
+# }
 
-if [[ "${ENABLE_UFW,,}" == "true" ]]; then
-  # enable firewall
-  echo "enabling firewall"
-  sed -i 's/IPV6=yes/IPV6=no/' /etc/default/ufw
-  ufw enable
+# if [[ "${ENABLE_UFW,,}" == "true" ]]; then
+#   # enable firewall
+#   echo "enabling firewall"
+#   sed -i 's/IPV6=yes/IPV6=no/' /etc/default/ufw
+#   ufw enable
 
-  if [[ -n "${UFW_EXTRA_PORTS}" ]]; then
-    for port in ${UFW_EXTRA_PORTS//,/ }; do
-      if [[ "${UFW_ALLOW_GW_NET,,}" == "true" ]]; then
-        ufwAllowPortLong "${port}" "${GW_CIDR}"
-      else
-        ufwAllowPortLong "${port}" "${GW}"
-      fi
-    done
-  fi
-fi
+#   if [[ -n "${UFW_EXTRA_PORTS}" ]]; then
+#     for port in ${UFW_EXTRA_PORTS//,/ }; do
+#       if [[ "${UFW_ALLOW_GW_NET,,}" == "true" ]]; then
+#         ufwAllowPortLong "${port}" "${GW_CIDR}"
+#       else
+#         ufwAllowPortLong "${port}" "${GW}"
+#       fi
+#     done
+#   fi
+# fi
 
-if [[ -n "${LOCAL_NETWORK}" ]]; then
-  if [[ -n "${GW}" ]] && [[ -n "${INT}" ]]; then
-    for localNet in ${LOCAL_NETWORK//,/ }; do
-      echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
-      # Using `ip route replace` so that the command does not fail with
-      # `RTNETLINK answers: File exists` when the route already exists
-      /sbin/ip route replace "${localNet}" via "${GW}" dev "${INT}"
-      if [[ "${ENABLE_UFW,,}" == "true" ]]; then
-        if [[ -n "${UFW_EXTRA_PORTS}" ]]; then
-          for port in ${UFW_EXTRA_PORTS//,/ }; do
-            ufwAllowPortLong "${port}" "${localNet}"
-          done
-        fi
-      fi
-    done
-  fi
-fi
+# if [[ -n "${LOCAL_NETWORK}" ]]; then
+#   if [[ -n "${GW}" ]] && [[ -n "${INT}" ]]; then
+#     for localNet in ${LOCAL_NETWORK//,/ }; do
+#       echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
+#       # Using `ip route replace` so that the command does not fail with
+#       # `RTNETLINK answers: File exists` when the route already exists
+#       /sbin/ip route replace "${localNet}" via "${GW}" dev "${INT}"
+#       if [[ "${ENABLE_UFW,,}" == "true" ]]; then
+#         if [[ -n "${UFW_EXTRA_PORTS}" ]]; then
+#           for port in ${UFW_EXTRA_PORTS//,/ }; do
+#             ufwAllowPortLong "${port}" "${localNet}"
+#           done
+#         fi
+#       fi
+#     done
+#   fi
+# fi
+
