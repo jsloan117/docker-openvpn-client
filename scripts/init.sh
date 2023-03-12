9942bca064881e23204becd908e7adcc0e744bf9
Jonathan Sloan <jsloan117@gmail.com>
2023-03-12 17:20:14 -0400

    fix typo
    Signed-off-by: Jonathan Sloan <jsloan117@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,7 +5,7 @@
 # shellcheck disable=SC2154
 if [[ ${VPN_CLIENT,,} == "openvpn" ]]; then
   cp -R /services/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d
-  cp /services/user/contents.d/{init-openvp,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d/user/contents.d
+  cp /services/user/contents.d/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d/user/contents.d
 elif [[ ${VPN_CLIENT,,} == "wireguard" ]]; then
   cp -R /services/svc-wireguard /etc/s6-overlay/s6-rc.d
   cp /services/user/contents.d/svc-wireguard /etc/s6-overlay/s6-rc.d/user/contents.d
