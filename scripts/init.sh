a9e5747189d1919bfc4e8ca8089971693d51ac41
Jonathan Sloan <jsloan117@gmail.com>
2023-08-27 11:32:32 -0400

    bump workflow task versions, attempt to dynamically use or not use ufw as a service
    Signed-off-by: Jonathan Sloan <jsloan117@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,10 +3,21 @@
 # called via "S6_STAGE2_HOOK" to setup openvpn or wireguard services
 
 # shellcheck disable=SC2154
-if [[ ${VPN_CLIENT,,} == "openvpn" ]]; then
-  cp -R /services/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d
-  cp /services/user/contents.d/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d/user/contents.d
-elif [[ ${VPN_CLIENT,,} == "wireguard" ]]; then
+if [[ ${VPN_CLIENT,,} = 'openvpn' ]]; then
+  if [[ ${ENABLE_UFW,,} = true ]]; then
+    services='init-openvpn,svc-ufw,svc-openvpn'
+  else
+    services='init-openvpn,svc-openvpn'
+  fi
+  # cp -R /services/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d
+  cp -R "/services/{${services}}" /etc/s6-overlay/s6-rc.d
+  if [[ ${ENABLE_UFW,,} = false ]]; then
+    rm -f /etc/s6-overlay/s6-rc.d/svc-openvpn/dependencies.d/svc-ufw
+    touch /etc/s6-overlay/s6-rc.d/svc-openvpn/dependencies.d/init-openvpn
+  fi
+  # cp /services/user/contents.d/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d/user/contents.d
+  cp "/services/user/contents.d/{${services}}" /etc/s6-overlay/s6-rc.d/user/contents.d
+elif [[ ${VPN_CLIENT,,} = 'wireguard' ]]; then
   cp -R /services/svc-wireguard /etc/s6-overlay/s6-rc.d
   cp /services/user/contents.d/svc-wireguard /etc/s6-overlay/s6-rc.d/user/contents.d
 fi
