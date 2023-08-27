500af680546024a2ffe10b03d0e15966f80daf3f
Jonathan Sloan <jsloan117@gmail.com>
2023-08-27 11:41:34 -0400

    adjust init.sh x2
    Signed-off-by: Jonathan Sloan <jsloan117@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,13 +10,13 @@ if [[ ${VPN_CLIENT,,} = 'openvpn' ]]; then
     services='{init-openvpn,svc-openvpn}'
   fi
   # cp -R /services/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d
-  cp -R "/services/${services}" /etc/s6-overlay/s6-rc.d
+  cp -R /services/${services} /etc/s6-overlay/s6-rc.d
   if [[ ${ENABLE_UFW,,} = false ]]; then
     rm -f /etc/s6-overlay/s6-rc.d/svc-openvpn/dependencies.d/svc-ufw
     touch /etc/s6-overlay/s6-rc.d/svc-openvpn/dependencies.d/init-openvpn
   fi
   # cp /services/user/contents.d/{init-openvpn,svc-ufw,svc-openvpn} /etc/s6-overlay/s6-rc.d/user/contents.d
-  cp "/services/user/contents.d/${services}" /etc/s6-overlay/s6-rc.d/user/contents.d
+  cp /services/user/contents.d/${services} /etc/s6-overlay/s6-rc.d/user/contents.d
 elif [[ ${VPN_CLIENT,,} = 'wireguard' ]]; then
   cp -R /services/svc-wireguard /etc/s6-overlay/s6-rc.d
   cp /services/user/contents.d/svc-wireguard /etc/s6-overlay/s6-rc.d/user/contents.d
